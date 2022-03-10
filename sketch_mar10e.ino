f24823a24093b2f8554920d5e0f296bc835d994a
IuvanChen <100844989+IuvanChen@users.noreply.github.com>
2022-03-10 21:19:36 +0800

    Add files via upload

---
+++ sketch_mar10e.ino
@@ -0,0 +1,27 @@
+#define ADC_ch A0
+#define ADC_level 1000
+#define sampleDelay 200
+unsigned long initTime;
+
+void setup(){
+   Serial.begin(9600);
+   analogSetPinAttenuation(ADC_ch,ADC_11db);
+   adcAttachPin(ADC_ch);
+
+   initTime=millis();
+   while(analogRead(ADC_ch)>ADC_level){
+   Serial.print(analogRead(ADC_ch));
+   Serial.println(millis()-initTime);
+   
+   }
+   Serial.println("......reading ADC .....");
+}
+
+   void loop(){
+    int adc_value;
+    adc_value=analogRead(ADC_ch);
+    Serial.println(adc_value);
+    delay(sampleDelay);
+    }
+   
+  
